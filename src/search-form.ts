330fa000ac7c021da095f9429a7929019630e1fd
Max <maxbui0911@gmail.com>
2022-01-14 00:21:47 +0200

    L1

---
+++ src/search-form.ts
@@ -0,0 +1,46 @@
+import { renderBlock } from './lib.js'
+//import moment from 'moment'
+
+export function renderSearchFormBlock(checkInDate: Date, checkOutDate: Date) {
+  let checkInDateStr = checkInDate.toISOString().slice(0, 10);
+  let checkOutDateStr = checkOutDate.toISOString().slice(0, 10);
+  let checkInDateDefault = new Date(checkInDate.setDate(checkInDate.getDate() + 1)).toISOString().slice(0, 10);
+  let checkOutDateDefault = new Date(checkInDate.setDate(checkInDate.getDate() + 2)).toISOString().slice(0, 10);
+  renderBlock(
+    'search-form-block',
+    `
+    <form>
+      <fieldset class="search-filedset">
+        <div class="row">
+          <div>
+            <label for="city">Город</label>
+            <input id="city" type="text" disabled value="Санкт-Петербург" />
+            <input type="hidden" disabled value="59.9386,30.3141" />
+          </div>
+          <!--<div class="providers">
+            <label><input type="checkbox" name="provider" value="homy" checked /> Homy</label>
+            <label><input type="checkbox" name="provider" value="flat-rent" checked /> FlatRent</label>
+          </div>--!>
+        </div>
+        <div class="row">
+          <div>
+            <label for="check-in-date">Дата заезда</label>
+            <input id="check-in-date" type="date" value="${checkInDateDefault}" min="${checkInDateStr}" max="${checkOutDateStr}" name="checkin" />
+          </div>
+          <div>
+            <label for="check-out-date">Дата выезда</label>
+            <input id="check-out-date" type="date" value="${checkOutDateDefault}" min="${checkInDateStr}" max="${checkOutDateStr}" name="checkout"/>
+            </div>
+            <div>
+              <label for="max-price">Макс. цена суток</label>
+              <input id="max-price" type="text" value="" name="price" class="max-price" />
+            </div>
+          <div>
+            <div><button>Найти</button></div>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+    `
+  )
+}
